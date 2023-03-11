c6a11a64705438ba190299139240ea25f1dba261
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-03-11 00:25:36 -0300

    Defines a theme and provides it to the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
 import App from 'components/App';
 
 import GlobalStyle from 'styles/global';
+import theme from 'styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    'black': '#050505',
+    'gray-darkest': '#1f1f1f',
+    'gray-darker': '#2d2d2d',
+    'gray-dark': '#3a3a3a',
+    'gray': '#757575',
+    'gray-lighter': '#e9e9e9',
+    'gray-lightest': '#f4f4f4',
+    'white': '#ffffff',
+    'purple': '#a445ed',
+    'red': '#ff5252'
+  }
+};
+
+export default theme;
