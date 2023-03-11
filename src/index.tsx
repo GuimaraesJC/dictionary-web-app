db8df2aef12f7fd32c9be1c84304c2ff1b069bf7
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-03-10 23:57:10 -0300

    Moves App.tsx to folder and sets alias for styles folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from 'components/App';
 
-import GlobalStyle from './styles/global';
+import GlobalStyle from 'styles/global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
